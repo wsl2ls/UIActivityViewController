05a8cad083a894c684efc932debd659a1ff0489a
wsl <15324956576@163.com>
2016-10-10 12:04:30 +0800

    yu
    uy

--- UIActivityViewController/ViewController.m
+++ UIActivityViewController/ViewController.m
@@ -3,7 +3,7 @@
 //  UIActivityViewController
 //
 //  Created by 王双龙 on 16/8/30.
-//  Copyright © 2016年 王双龙. All rights reserved.
+//  Copyright © 2016年 http://www.jianshu.com/users/e15d1f644bea All rights reserved.
 //
 
 #import "ViewController.h"
